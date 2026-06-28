139496432bcd39502c565eab862ee993d54c6eee
Ali Ghadimi <alighadimi787@gmail.com>
2026-06-28 17:13:52 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
+fetch("data/websites.json")
+    .then(response => response.json())
+    .then(websites => {
 
+        const container = document.getElementById("portfolio-container");
+
+        websites.forEach(site => {
+
+            container.innerHTML += `
+
+                <div class="portfolio-card">
+
+                    <img src="${site.image}" alt="${site.title}">
+
+                    <div class="portfolio-content">
+
+                        <span class="category">
+                            ${site.category}
+                        </span>
+
+                        <h3>
+                            ${site.title}
+                        </h3>
+
+                        <p>
+                            ${site.description}
+                        </p>
+
+                        <div class="price">
+
+                            ${site.price}
+
+                        </div>
+
+                        <div class="buttons">
+
+                            <a href="${site.demo}" target="_blank">
+
+                                مشاهده دمو
+
+                            </a>
+
+                            <a href="#order">
+
+                                سفارش مشابه
+
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            `;
+
+        });
+
+    });
